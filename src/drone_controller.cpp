ea2967456ae3827ce4dc1031829428fb092a9f38
Luiz Miguel Tavares <luizmiguel.pessotti@gmail.com>
2024-12-02 15:42:51 -0300

    Basic changes

--- src/drone_controller.cpp
+++ src/drone_controller.cpp
@@ -13,70 +13,60 @@ class DroneController
 {
 public:
     DroneController() : nh_("~")
-    {
-        // Initialize subscribers and publishers
-        pose_sub_ = nh_.subscribe("/natnet_ros/B1/pose", 1, &DroneController::poseCallback, this);
-        // odometry_sub_ = nh_.subscribe("/B1/odom", 1, &DroneController::odometryCallback, this);
+    {   
+        // Perder o corpo, paredes virtuais, joystick, gráficos.
 
+        // Params
+        nh_.param("save_data", save_data_, true);
+        nh_.param("pose_topic", pose_topic_, std::string("/natnet_ros/B1/pose"));
+        nh_.param("cmd_vel_topic", cmd_vel_topic_, std::string("/B1/cmd_vel"));
+        nh_.param("takeoff_topic", takeoff_topic_, std::string("/B1/takeoff"));
+        nh_.param("land_topic", land_topic_, std::string("/B1/land"));
+        nh_.param("control_frequency", control_frequency_, 30.0); 
+
+        // Gains
+        nh_.param("Kp_x", Kp_(0, 0), 1.0);
+        nh_.param("Kp_y", Kp_(1, 1), 1.0);
+        nh_.param("Kd_x", Kd_(0, 0), 1.0);
+        nh_.param("Kd_y", Kd_(1, 1), 1.0);
+        nh_.param("Ki_x", Ki_(0, 0), 0.0);
+        nh_.param("Ki_y", Ki_(1, 1), 0.0);
+        nh_.param("K_z", K_z_, 1.0);
+        nh_.param("K_yaw", K_yaw_, 5.0);
+        nh_.param("alpha_velocity_gain", alpha_velocity_gain_, 0.4);
+        nh_.param("theta_phi_max_deg", max_theta_phi_deg_, 15.0);
+        nh_.param("z_dot_max", max_z_dot_, 1.0);
+        nh_.param("yaw_dot_max_deg", max_yaw_dot_deg_, 100.0);
+        nh_.param("Angular_frequency", w_, 2 * M_PI / 20); // Angular frequency
+
+        // Task
+        nh_.param("task", task_, 1); // 1: Positioning, 2: Circular trajectory, 3: Lemniscate trajectory, 4: Pringles trajectory
+
+        pose_sub_ = nh_.subscribe(pose_topic_, 1, &DroneController::poseCallback, this);
         cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("/B1/cmd_vel", 1);
         takeoff_pub_ = nh_.advertise<std_msgs::Empty>("/B1/takeoff", 1);
         land_pub_ = nh_.advertise<std_msgs::Empty>("/B1/land", 1);
-
-        // Input variables
-        nh_.param("save_data", save_data_, true);
+        
         if (save_data_)
         {   
-            //getting the date and time to create a unique file name
-            time_t now = time(0);
-            tm *ltm = localtime(&now);
-            std::string file_name = "drone_data_" + std::to_string(1900 + ltm->tm_year) + "_" + std::to_string(1 + ltm->tm_mon) + "_" + std::to_string(ltm->tm_mday) + "_" + std::to_string(ltm->tm_hour) + "_" + std::to_string(ltm->tm_min) + "_" + std::to_string(ltm->tm_sec) + ".csv";
-            csv_file_.open(file_name);
-            if (csv_file_.is_open())
-            {
-                csv_file_ << "Time,dt,Pos_X,Pos_Y,Pos_Z,Vel_X,Vel_Y,Vel_Z\n";
-            }
-            else
-            {
-                ROS_ERROR("Unable to open CSV file for writing.");
-            }
-            ROS_INFO_STREAM("Saving data to CSV file in the current directory with the name: " << file_name);     
+            save_data();
         }
 
         there_is_pose_ = false;
 
         // Initialize control parameters
-        control_frequency_ = 30.0; // Hz
         dt_ = 1.0 / control_frequency_;
-
-        // Control gains
-        Kp_ = Eigen::Matrix2d::Identity();
-        Kd_ = Eigen::Matrix2d::Identity();
-        K_z_ = 1.0;
-        K_yaw_ = 5.0;
-        alpha_velocity_gain_ = 0.4;
-
-        // Initialize other parameters
-        mass_model_ = 10.0;
-        mass_estimated_ = 10.0;
         g_ = 9.81;
-        t_ = mass_model_ * g_;
 
-        Ku_ = (t_ / mass_model_) * Eigen::Matrix2d::Identity();
-        Ku_inv_control_ = (mass_estimated_ / t_) * Eigen::Matrix2d::Identity();
+        Ku_inv_control_ = (1 / g_) * Eigen::Matrix2d::Identity();
 
-        max_theta_phi_deg_ = 15.0;
         max_theta_phi_ = deg2rad(max_theta_phi_deg_);
-
-        max_z_dot_ = 1.0;
-        max_yaw_dot_deg_ = 100.0;
         max_yaw_dot_ = deg2rad(max_yaw_dot_deg_);
-
-        w_ = 2 * M_PI / 20; // Angular frequency
         
         // Set up the control loop timer
         control_timer_ = nh_.createTimer(ros::Duration(dt_), &DroneController::controlLoop, this);
 
-        first_time_ = ros::Time::now();
+        initial_time_ = ros::Time::now();
         previous_pose_time_ = ros::Time::now();
 
     }
@@ -103,12 +93,6 @@ private:
         }
 
         double dt = (current_time - previous_pose_time_).toSec();
-        // ROS_INFO("dt: %f", dt);
-
-        // if (dt < 0.016667){
-        //     ROS_INFO("Pose received too fast. Skipping...");
-        //     return;
-        // }
 
         current_position_ = Eigen::Vector3d(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
         tf::quaternionMsgToTF(msg->pose.orientation, current_orientation_);
@@ -133,57 +117,6 @@ private:
 
     }
 
-    // void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg)
-    // {
-    //     std::lock_guard<std::mutex> lock(state_mutex_);
-    //     ros::Time current_time = msg->header.stamp;
-
-    //     if (there_is_pose_ == false)
-    //     {   
-    //         current_position_ = Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
-    //         previous_position_ = current_position_;
-    //         previous_pose_time_ = current_time;
-
-    //         current_velocity_ = Eigen::Vector3d::Zero();
-    //         previous_velocity_ = current_velocity_;
-
-    //         tf::quaternionMsgToTF(msg->pose.pose.orientation, current_orientation_);
-
-    //         there_is_pose_ = true;
-    //         return;
-    //     }
-
-    //     double dt = (current_time - previous_pose_time_).toSec();
-    //     // ROS_INFO("dt: %f", dt);
-
-    //     // if (dt < 0.016667){
-    //     //     ROS_INFO("Pose received too fast. Skipping...");
-    //     //     return;
-    //     // }
-
-    //     current_position_ = Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
-    //     tf::quaternionMsgToTF(msg->pose.pose.orientation, current_orientation_);
-
-    //     if (dt > 0.0)
-    //     {
-    //         Eigen::Vector3d new_velocity_ = (current_position_ - previous_position_) / dt;
-
-    //         current_velocity_ = alpha_velocity_gain_ * new_velocity_ + (1 - alpha_velocity_gain_) * previous_velocity_;
-
-    //         previous_velocity_ = current_velocity_;
-    //         previous_position_ = current_position_;
-    //         previous_pose_time_ = current_time;
-    //     } 
-
-    //     if (save_data_ && csv_file_.is_open())
-    //     {
-    //         csv_file_ << current_time << "," << dt << ","
-    //                   << current_position_(0) << "," << current_position_(1) << "," << current_position_(2) << ","
-    //                   << current_velocity_(0) << "," << current_velocity_(1) << "," << current_velocity_(2) << "\n";
-    //     }
-
-    // }
-
     void controlLoop(const ros::TimerEvent& event)
     {   
         if (!there_is_pose_)
@@ -207,7 +140,7 @@ private:
         }
 
         ros::Time current_time = ros::Time::now();
-        double t = (current_time - first_time_).toSec();
+        double t = (current_time - initial_time_).toSec();
 
         // Compute desired states
         Eigen::Vector3d X_des = computeDesiredPosition(t, w_);
@@ -253,6 +186,24 @@ private:
         // ROS_INFO("X_deg: %f, Y_deg: %f, Z_dot : %f, Yaw rate: %f", theta_phi_ref(0), theta_phi_ref(1), Z_dot_ref, yaw_dot_ref);
 
         // cmd_pub_.publish(cmd_msg);
+    }
+
+    void save_data()
+    {   
+        //getting the date and time to create a unique file name
+        time_t now = time(0);
+        tm *ltm = localtime(&now);
+        std::string file_name = "drone_data_" + std::to_string(1900 + ltm->tm_year) + "_" + std::to_string(1 + ltm->tm_mon) + "_" + std::to_string(ltm->tm_mday) + "_" + std::to_string(ltm->tm_hour) + "_" + std::to_string(ltm->tm_min) + "_" + std::to_string(ltm->tm_sec) + ".csv";
+        csv_file_.open(file_name);
+        if (csv_file_.is_open())
+        {
+            csv_file_ << "Time,dt,Pos_X,Pos_Y,Pos_Z,Vel_X,Vel_Y,Vel_Z\n";
+        }
+        else
+        {
+            ROS_ERROR("Unable to open CSV file for writing.");
+        }
+        ROS_INFO_STREAM("Saving data to CSV file in the current directory with the name: " << file_name);     
     }
 
     Eigen::Vector3d computeDesiredPosition(double t, double w)
@@ -306,6 +257,8 @@ private:
 
     // Member variables
     bool there_is_pose_;
+    std::string pose_topic_, cmd_vel_topic_, takeoff_topic_, land_topic_;
+    int task_;
     ros::NodeHandle nh_;
     ros::Subscriber pose_sub_, odometry_sub_;
     ros::Publisher cmd_pub_;
@@ -331,6 +284,7 @@ private:
 
     Eigen::Matrix2d Kp_;
     Eigen::Matrix2d Kd_;
+    Eigen::Matrix2d Ki_;
     double K_z_;
     double K_yaw_;
     double alpha_velocity_gain_;
@@ -349,7 +303,7 @@ private:
 
     double w_; // Angular frequency
 
-    ros::Time first_time_;
+    ros::Time initial_time_;
     bool save_data_;
     std::ofstream csv_file_;
 };
